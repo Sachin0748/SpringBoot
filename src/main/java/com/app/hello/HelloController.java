a0d4264172dad62c0228827fc58634178a7db9bb
Sachin Kumar Thakur <kumarsachinsk456@gmail.com>
2025-02-27 11:26:32 +0530

    [Sachin Kumar Thakur] Add. Spring intro UC_02

--- src/main/java/com/app/hello/HelloController.java
+++ src/main/java/com/app/hello/HelloController.java
@@ -2,14 +2,22 @@ package com.app.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-public class UC1 {
+public class HelloController {
 
     @GetMapping("/hello1")
     public String hello1(){
         return "Hello from BridgeLabz.";
     }
+
+    @RequestMapping("/hello2")
+    public String hello2(@RequestParam String name){
+        return "Hello " + name + " from BridgeLabz.";
+    }
+
+    
 }

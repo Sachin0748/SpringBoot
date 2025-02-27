6445ad30199f96385f31cb1c355387722205e832
Sachin Kumar Thakur <kumarsachinsk456@gmail.com>
2025-02-27 11:45:57 +0530

    [Sachin] Add. Spring Intro UC_03

--- src/main/java/com/app/hello/HelloController.java
+++ src/main/java/com/app/hello/HelloController.java
@@ -6,14 +6,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello")
 public class HelloController {
 
+    // UC_01
     @GetMapping("/hello1")
     public String hello1(){
         return "Hello from BridgeLabz.";
     }
 
+    // UC_02
     @GetMapping("/hello2")
     public String hello2(@RequestParam String name){
         return "Hello " + name + " from BridgeLabz.";
     }
 
+    // UC_03
+    @GetMapping("/hello3/{name}")
+    public String hello3(@PathVariable String name){
+        return "Hello " + name + " from BridgeLabz.";
+    }
 }

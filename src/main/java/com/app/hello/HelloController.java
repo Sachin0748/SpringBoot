cfd4bab585db5e2a2cac6eda40c43f028af4758f
Sachin Kumar Thakur <kumarsachinsk456@gmail.com>
2025-02-27 11:36:28 +0530

    [Sachin] Refactor .Sprint Intro UC_02

--- src/main/java/com/app/hello/HelloController.java
+++ src/main/java/com/app/hello/HelloController.java
@@ -1,9 +1,6 @@
 package com.app.hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -14,10 +11,9 @@ public class HelloController {
         return "Hello from BridgeLabz.";
     }
 
-    @RequestMapping("/hello2")
+    @GetMapping("/hello2")
     public String hello2(@RequestParam String name){
         return "Hello " + name + " from BridgeLabz.";
     }
 
-    
 }
